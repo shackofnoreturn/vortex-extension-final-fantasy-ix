e501b908e8500a6bae996b0d84b974a9cc6ba379
Jens Ackou <shackofnoreturn@gmail.com>
2024-04-05 18:37:03 +0200

    Reworked to simpler search method

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const Promise = require('bluebird');
-const path = require('path');
 const { util } = require('vortex-api');
 const GAME_ID 	= 'finalfantasy9';
 const GAME_NAME	= 'Final Fantasy IX';
 const STEAMAPP_ID = '377840';
 
-function requiresLauncher(gamePath) {
-	return util.GameStoreHelper.findByAppId([STEAMAPP_ID], 'xbox')
-	  .then(() => Promise.resolve({
-		launcher: 'xbox',
-		addInfo: {
-		  appId: STEAMAPP_ID,
-		  parameters: [
-			{ appExecName: 'Game' },
-		  ],
-		}
-	  }))
-	  .catch(err => Promise.resolve(undefined));
+function findGame() {
+	return util.steam.findByAppId(STEAMAPP_ID)
+	  .then(game => game.gamePath);
 }
 
 function main(context) {
@@ -24,19 +13,15 @@ function main(context) {
 		id: GAME_ID,
 		name: GAME_NAME,
 		mergeMods: true,
-		queryArgs: {
-			steam: [{ name: GAME_NAME }],
-		},
+		queryPath: findGame,
 		supportedTools: [],
 		queryModPath: () => '',
 		logo: 'gameart.jpg',
 		executable: () => 'FF9_Launcher.exe',
 		requiredFiles: [
 		  'FF9_Launcher.exe',
-		  'x64/FF9.exe',
-		  'x86/FF9.exe'
+		  'x64/FF9.exe'
 		],
-		requiresLauncher,
 		environment: {
 		  SteamAPPId: STEAMAPP_ID,
 		},
